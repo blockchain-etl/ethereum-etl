1079219e98d93bd5b1744596d18add233c5d5791
Evgeny Medvedev <medvedev1088@gmail.com>
2018-05-06 16:17:01 +0700

    Use web3

---
+++ ethereumetl/boundedexecutor.py
@@ -0,0 +1,30 @@
+from concurrent.futures import ThreadPoolExecutor
+from threading import BoundedSemaphore
+
+
+class BoundedExecutor:
+    """BoundedExecutor behaves as a ThreadPoolExecutor which will block on
+    calls to submit() once the limit given as "bound" work items are queued for
+    execution.
+    :param bound: Integer - the maximum number of items in the work queue
+    :param max_workers: Integer - the size of the thread pool
+    """
+    def __init__(self, bound, max_workers):
+        self.executor = ThreadPoolExecutor(max_workers=max_workers)
+        self.semaphore = BoundedSemaphore(bound + max_workers)
+
+    """See concurrent.futures.Executor#submit"""
+    def submit(self, fn, *args, **kwargs):
+        self.semaphore.acquire()
+        try:
+            future = self.executor.submit(fn, *args, **kwargs)
+        except:
+            self.semaphore.release()
+            raise
+        else:
+            future.add_done_callback(lambda x: self.semaphore.release())
+            return future
+
+    """See concurrent.futures.Executor#shutdown"""
+    def shutdown(self, wait=True):
+        self.executor.shutdown(wait)
--- exchange_with_socket.py
+++ exchange_with_socket.py
@@ -1,30 +1,29 @@
 import argparse
+import json
+import threading
 
-from ethereumetl.socket_utils import exchange_with_retries
-from ethereumetl.utils import smart_open, batch_readlines
+from web3 import IPCProvider
+
+from ethereumetl.utils import smart_open
 
 parser = argparse.ArgumentParser(
     description='Sends the given input to the socket and outputs its response')
 parser.add_argument('--input', default=None, type=str, help='The input file. If not specified stdin is used.')
 parser.add_argument('--output', default=None, type=str, help='The output file. If not specified stdout is used.')
 parser.add_argument('--socket', required=True, type=str, help='The full path to the socket.')
-# Use 1 for parity as it ignores some requests and behaves unreliable.
-# Depending on RAM size use up to 100000 for geth.
 parser.add_argument('--batch-size', default=1, type=int, help='The number of lines in the batch to send to socket.')
-parser.add_argument('--max-retries', default=10, type=int,
-                    help='The number of retries in case an exception is encountered.')
-parser.add_argument('--timeout-seconds', default=10, type=int,
-                    help='The number of seconds to wait for response from socket before throwing timeout.')
+
 
 args = parser.parse_args()
 
-is_parity = "parity" in args.socket.lower()
-
 with smart_open(args.input, 'r') as input_file, smart_open(args.output) as output_file:
-    for lines_batch in batch_readlines(input_file, args.batch_size):
-        lines = ''.join(lines_batch)
-        if is_parity:
-            # Parity IPC somehow ignores the last line
-            lines += '{}\n'
-        response = exchange_with_retries(args.socket, lines, args.max_retries, args.timeout_seconds)
-        output_file.write(response)
+    output_file_lock = threading.Lock()
+
+    for line in input_file.readlines():
+        request = json.loads(line)
+        ipc_provider = IPCProvider(args.socket)
+        response = ipc_provider.make_request(request['method'], request['params'])
+
+        with output_file_lock:
+            output_file.write(json.dumps(response) + '\n')
+
