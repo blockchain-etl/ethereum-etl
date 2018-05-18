aa2ab9567d55b8a4380d9943190fa368400774a5
evgeny <medvedev1088@gmail.com>
2018-05-18 18:14:22 +0700

    Add ExportErc20TransfersJob

--- ethereumetl/jobs.py
+++ ethereumetl/jobs.py
@@ -6,7 +6,10 @@ from ethereumetl.exporters import CsvItemExporter
 from ethereumetl.file_utils import get_file_handle
 from ethereumetl.json_rpc_requests import generate_get_block_by_number_json_rpc
 from ethereumetl.mapper.block_mapper import EthBlockMapper
+from ethereumetl.mapper.erc20_transfer_mapper import EthErc20TransferMapper
 from ethereumetl.mapper.transaction_mapper import EthTransactionMapper
+from ethereumetl.mapper.transaction_receipt_log_mapper import EthReceiptLogMapper
+from ethereumetl.service.erc20_processor import EthErc20Processor, TRANSFER_EVENT_TOPIC
 from ethereumetl.utils import split_to_batches
 
 
@@ -79,3 +82,65 @@ class ExportBlocksJob(object):
             self.blocks_output_file.close()
         if self.transactions_output_file is not None:
             self.transactions_output_file.close()
+
+
+class ExportErc20TransfersJob(object):
+    def __init__(self,
+                 start_block,
+                 end_block,
+                 batch_size,
+                 web3,
+                 output):
+        self.start_block = start_block
+        self.end_block = end_block
+        self.batch_size = batch_size
+        self.web3 = web3
+        self.output = output
+
+        self.receipt_log_mapper = EthReceiptLogMapper()
+        self.erc20_transfer_mapper = EthErc20TransferMapper()
+        self.erc20_processor = EthErc20Processor()
+
+        self.output_file = None
+        self.exporter = None
+
+    def run(self):
+        try:
+            self._start()
+            self._export()
+        finally:
+            self._end()
+
+    def _start(self):
+        self.output_file = get_file_handle(self.output, binary=True)
+        self.exporter = CsvItemExporter(self.output_file)
+
+    def _export(self):
+        for batch_start, batch_end in split_to_batches(self.start_block, self.end_block, self.batch_size):
+            try:
+                self._export_batch(batch_start, batch_end)
+            except Timeout:
+                # try exporting blocks one by one
+                for block_number in range(batch_start, batch_end + 1):
+                    self._export_batch(block_number, block_number)
+
+    def _export_batch(self, batch_start, batch_end):
+        event_filter = self.web3.eth.filter({
+            "fromBlock": batch_start,
+            "toBlock": batch_end,
+            "topics": [TRANSFER_EVENT_TOPIC]
+        })
+
+        events = event_filter.get_all_entries()
+
+        for event in events:
+            log = self.receipt_log_mapper.web3_dict_to_transaction_receipt_log(event)
+            erc20_transfer = self.erc20_processor.filter_transfer_from_log(log)
+            if erc20_transfer is not None:
+                self.exporter.export_item(self.erc20_transfer_mapper.erc20_transfer_to_dict(erc20_transfer))
+
+        self.web3.eth.uninstallFilter(event_filter.filter_id)
+
+    def _end(self):
+        if self.output_file is not None:
+            self.output_file.close()
--- export_erc20_transfers.py
+++ export_erc20_transfers.py
@@ -2,47 +2,23 @@ import argparse
 
 from web3 import IPCProvider, Web3
 
-from ethereumetl.exporters import CsvItemExporter
-from ethereumetl.mapper.erc20_transfer_mapper import EthErc20TransferMapper
-from ethereumetl.mapper.transaction_receipt_log_mapper import EthReceiptLogMapper
-from ethereumetl.service.erc20_processor import EthErc20Processor, TRANSFER_EVENT_TOPIC
-from ethereumetl.utils import smart_open
+from ethereumetl.jobs import ExportErc20TransfersJob
 
 parser = argparse.ArgumentParser(
     description='Exports ERC20 transfers using eth_newFilter and eth_getFilterLogs JSON RPC APIs.')
 parser.add_argument('--start-block', default=0, type=int, help='Start block')
 parser.add_argument('--end-block', required=True, type=int, help='End block')
-parser.add_argument('--output', default=None, type=str, help='The output file. If not specified stdout is used.')
+parser.add_argument('--output', default='-', type=str, help='The output file. If not specified stdout is used.')
 parser.add_argument('--ipc-path', required=True, type=str, help='The full path to the ipc socket file.')
 parser.add_argument('--ipc-timeout', default=300, type=int, help='The timeout in seconds for ipc calls.')
 parser.add_argument('--batch-size', default=100, type=int, help='The number of blocks to filter at a time.')
 
 args = parser.parse_args()
 
+job = ExportErc20TransfersJob(start_block=args.start_block,
+                              end_block=args.end_block,
+                              batch_size=args.batch_size,
+                              web3=Web3(IPCProvider(args.ipc_path, timeout=args.ipc_timeout)),
+                              output=args.output)
 
-with smart_open(args.output, binary=True) as output_file:
-    transaction_receipt_log_mapper = EthReceiptLogMapper()
-    erc20_transfer_mapper = EthErc20TransferMapper()
-    erc20_processor = EthErc20Processor()
-    exporter = CsvItemExporter(output_file)
-
-    web3 = Web3(IPCProvider(args.ipc_path, timeout=args.ipc_timeout))
-
-    for batch_start_block in range(args.start_block, args.end_block + 1, args.batch_size):
-        batch_end_block = min(batch_start_block + args.batch_size - 1, args.end_block)
-
-        event_filter = web3.eth.filter({
-            "fromBlock": batch_start_block,
-            "toBlock": batch_end_block,
-            "topics": [TRANSFER_EVENT_TOPIC]
-        })
-
-        events = event_filter.get_all_entries()
-
-        for event in events:
-            log = transaction_receipt_log_mapper.web3_dict_to_transaction_receipt_log(event)
-            erc20_transfer = erc20_processor.filter_transfer_from_log(log)
-            if erc20_transfer is not None:
-                exporter.export_item(erc20_transfer_mapper.erc20_transfer_to_dict(erc20_transfer))
-
-        web3.eth.uninstallFilter(event_filter.filter_id)
+job.run()
