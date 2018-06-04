0083e28678b002475dcbe43e28f3435988fec465
Evgeny Medvedev <medvedev1088@gmail.com>
2018-06-05 00:47:38 +0700

    Change export jobs

--- ethereum-export-pipeline/ethereum-export-pipeline.py
+++ ethereum-export-pipeline/ethereum-export-pipeline.py
@@ -21,8 +21,14 @@ S3Bucket = t.add_parameter(Parameter(
 
 # The first million blocks are in a single partition
 # The next 3 million blocks are in 100k partitions
-# The next 2 million blocks are in 10k partitions
-export_jobs = [(start, end, 10000) for start, end in split_to_batches(5000000, 5699999, 10000)]
+# The next 1 million blocks are in 10k partitions
+# Note that there is a limit in Data Pipeline on the number of objects that can be increased in Support Center
+# https://docs.aws.amazon.com/datapipeline/latest/DeveloperGuide/dp-limits.html
+export_jobs = [(0, 999999, 1000000)] + \
+              [(start, end, 100000) for start, end in split_to_batches(1000000, 1999999, 1000000)] + \
+              [(start, end, 100000) for start, end in split_to_batches(2000000, 2999999, 1000000)] + \
+              [(start, end, 100000) for start, end in split_to_batches(3000000, 3999999, 1000000)] + \
+              [(start, end, 10000) for start, end in split_to_batches(4000000, 4999999, 10000)]
 
 t.add_resource(Pipeline(
     "EthereumETLPipeline",
