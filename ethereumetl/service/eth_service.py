f71129b6ff095d8d24217deea37646e3fa1bd33e
evgeny <medvedev1088@gmail.com>
2018-07-13 17:57:55 +0700

    Simplify get_blocks_for_timestamp

--- ethereumetl/service/eth_service.py
+++ ethereumetl/service/eth_service.py
@@ -31,35 +31,26 @@ class EthService(object):
         elif timestamp == end_timestamp:
             return end_number, end_number
         elif (end_number - start_number) <= 1:
-            print(start_number, end_number)
             return start_number, end_number
         else:
             assert start_timestamp <= timestamp <= end_timestamp
             # Block numbers must increase strictly monotonically
             assert start_timestamp < end_timestamp
-            timestamp_difference = end_timestamp - start_timestamp
-            timestamp_increment = timestamp - start_timestamp
-            number_difference = end_number - start_number
-            estimated_number_increment = int(timestamp_increment * number_difference / timestamp_difference)
 
-            estimated_number = start_number + estimated_number_increment
-            estimated_block = self._web3.eth.getBlock(estimated_number)
-            self._request_count = self._request_count + 1
-            estimated_timestamp = estimated_block.timestamp
-
+            # Uses dichotomy method, complexity log(n) where n is the number of blocks
+            # TODO: Optimize it by using gradient decent
             middle_number = start_number + int((end_number - start_number) / 2)
             middle_block = self._web3.eth.getBlock(middle_number)
             self._request_count = self._request_count + 1
             middle_timestamp = middle_block.timestamp
 
-            all_points = [(start_timestamp, start_block), (middle_timestamp, middle_block),
-                          (estimated_timestamp, estimated_block), (end_timestamp, end_block)]
+            all_points = [(start_timestamp, start_block), (middle_timestamp, middle_block), (end_timestamp, end_block)]
 
             sorted_points = sorted(all_points, key=lambda x: x[0])
 
-            for pair in pairwise(sorted_points):
-                if pair[0][0] <= timestamp <= pair[1][0]:
-                    new_block_range = pair[0][1], pair[1][1]
+            for (first_timestamp, first_block), (second_timestamp, second_block) in pairwise(sorted_points):
+                if first_timestamp <= timestamp <= second_timestamp:
+                    new_block_range = first_block, second_block
                     break
             else:
                 raise ValueError('Something is wrong')
