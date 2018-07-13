04ed441f0dadd1e7bf778aaa3b3dd8a394ce2b0d
evgeny <medvedev1088@gmail.com>
2018-07-13 18:50:02 +0700

    Add edge case handling

--- ethereumetl/service/eth_service.py
+++ ethereumetl/service/eth_service.py
@@ -2,11 +2,27 @@ class EthService(object):
     def __init__(self, web3):
         self._web3 = web3
 
-    def get_block_range_for_timestamps(self, timestamp_start, timestamp_end):
-        start_blocks = self.get_block_number_bounds_for_timestamp(timestamp_start)
-        end_blocks = self.get_block_number_bounds_for_timestamp(timestamp_end)
+    def get_block_range_for_timestamps(self, start_timestamp, end_timestamp):
+        if start_timestamp > end_timestamp:
+            raise ValueError('start_timestamp must be greater or equal to end_timestamp')
 
-        return start_blocks[1], end_blocks[0]
+        try:
+            start_block_bounds = self.get_block_number_bounds_for_timestamp(start_timestamp)
+        except OutOfBounds:
+            start_block_bounds = (0, 0)
+
+        end_block_bounds = self.get_block_number_bounds_for_timestamp(end_timestamp)
+
+        if start_block_bounds == end_block_bounds and start_block_bounds[0] != start_block_bounds[1]:
+            raise ValueError('The given timestamp range does not cover any blocks')
+
+        start_block = start_block_bounds[1]
+        end_block = end_block_bounds[0]
+
+        if start_block == 1:
+            start_block = 0
+
+        return start_block, end_block
 
     def get_block_number_bounds_for_timestamp(self, timestamp):
         start_block = self._web3.eth.getBlock(1)
@@ -20,8 +36,8 @@ class EthService(object):
         start_timestamp, end_timestamp = start_block.timestamp, end_block.timestamp
 
         if timestamp < start_timestamp or timestamp > end_timestamp:
-            raise ValueError('timestamp {} is out of bounds for blocks {}-{}, timestamps {}-{}'
-                             .format(timestamp, start_number, end_number, start_timestamp, end_timestamp))
+            raise OutOfBounds('timestamp {} is out of bounds for blocks {}-{}, timestamps {}-{}'
+                              .format(timestamp, start_number, end_number, start_timestamp, end_timestamp))
 
         if timestamp == start_timestamp:
             return start_number, start_number
@@ -46,3 +62,7 @@ class EthService(object):
                 new_block_range = start_block, middle_block
 
             return self._get_block_number_bounds_for_timestamp_recursive(timestamp, *new_block_range)
+
+
+class OutOfBounds(Exception):
+    pass
