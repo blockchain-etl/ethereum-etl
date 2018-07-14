8fe9649b08138f340dd322ce8c554570e96aa386
Evgeny Medvedev <medvedev1088@gmail.com>
2018-07-15 01:29:56 +0700

    Fix tests

--- tests/ethereumetl/service/test_eth_service.py
+++ tests/ethereumetl/service/test_eth_service.py
@@ -1,50 +1,64 @@
 import os
-from datetime import date
 
 import pytest
+from dateutil.parser import parse
 from web3 import HTTPProvider, Web3
 
 from ethereumetl.service.eth_service import EthService
 from ethereumetl.service.graph_operations import OutOfBoundsError
 
-skip_long_tests = os.environ.get('ETHEREUM_ETL_RUN_LONG_TESTS', None) != '1'
-skip = pytest.mark.skipif(skip_long_tests, reason='Long running tests')
-
-web3 = Web3(HTTPProvider('https://mainnet.infura.io/'))
-eth_service = EthService(web3)
+run_slow_tests = os.environ.get('ETHEREUM_ETL_RUN_SLOW_TESTS', None) == '1'
+skip_slow_tests = pytest.mark.skipif(not run_slow_tests, reason='Slow running tests')
 
 
-@skip
-def test_get_block_range_for_date():
-    blocks = eth_service.get_block_range_for_date(date(2015, 7, 30))
-    assert blocks == (0, 6911)
+@skip_slow_tests
+@pytest.mark.parametrize("date,expected_start_block,expected_end_block", [
+    ('2015-07-30', 0, 6911),
+    ('2015-07-31', 6912, 13774),
+    ('2017-01-01', 2912407, 2918517),
+    ('2017-01-02', 2918518, 2924575),
+    ('2018-06-10', 5761663, 5767303)
+])
+def test_get_block_range_for_date(date, expected_start_block, expected_end_block):
+    eth_service = get_new_eth_service()
+    parsed_date = parse(date)
+    blocks = eth_service.get_block_range_for_date(parsed_date)
+    assert blocks == (expected_start_block, expected_end_block)
 
-    blocks = eth_service.get_block_range_for_date(date(2015, 7, 31))
-    assert blocks == (6912, 13774)
 
-    blocks = eth_service.get_block_range_for_date(date(2017, 1, 1))
-    assert blocks == (2912407, 2918517)
-
-    blocks = eth_service.get_block_range_for_date(date(2017, 1, 2))
-    assert blocks == (2918518, 2924575)
-
-    blocks = eth_service.get_block_range_for_date(date(2018, 6, 10))
-    assert blocks == (5761663, 5767303)
-
+@skip_slow_tests
+@pytest.mark.parametrize("date", [
+    '2015-07-29',
+    '2030-01-01'
+])
+def test_get_block_range_for_date_fail(date):
+    eth_service = get_new_eth_service()
+    parsed_date = parse(date)
     with pytest.raises(OutOfBoundsError):
-        eth_service.get_block_range_for_date(date(2015, 7, 29))
-
-    with pytest.raises(OutOfBoundsError):
-        eth_service.get_block_range_for_date(date(2030, 1, 1))
+        eth_service.get_block_range_for_date(parsed_date)
 
 
-@skip
-def test_get_block_range_for_timestamps():
-    blocks = eth_service.get_block_range_for_timestamps(1438270128, 1438270128)
-    assert blocks == (10, 10)
+@skip_slow_tests
+@pytest.mark.parametrize("start_timestamp,end_timestamp,expected_start_block,expected_end_block", [
+    (1438270128, 1438270128, 10, 10),
+    (1438270128, 1438270129, 10, 10)
+])
+def test_get_block_range_for_timestamps(start_timestamp, end_timestamp, expected_start_block, expected_end_block):
+    eth_service = get_new_eth_service()
+    blocks = eth_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
+    assert blocks == (expected_start_block, expected_end_block)
 
-    blocks = eth_service.get_block_range_for_timestamps(1438270128, 1438270129)
-    assert blocks == (10, 10)
 
+@skip_slow_tests
+@pytest.mark.parametrize("start_timestamp,end_timestamp", [
+    (1438270129, 1438270131)
+])
+def test_get_block_range_for_timestamps_fail(start_timestamp, end_timestamp):
+    eth_service = get_new_eth_service()
     with pytest.raises(ValueError):
-        eth_service.get_block_range_for_timestamps(1438270129, 1438270131)
+        eth_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
+
+
+def get_new_eth_service():
+    web3 = Web3(HTTPProvider('https://mainnet.infura.io/'))
+    return EthService(web3)
