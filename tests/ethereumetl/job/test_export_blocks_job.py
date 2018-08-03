f741adf5ede902f69bd2482602f392b983ad042d
Evgeny Medvedev <medvedev1088@gmail.com>
2018-08-04 00:06:13 +0700

    Add Infura tests

--- tests/ethereumetl/job/test_export_blocks_job.py
+++ tests/ethereumetl/job/test_export_blocks_job.py
@@ -20,15 +20,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 import pytest
 
 import tests.resources
 from ethereumetl.jobs.export_blocks_job import ExportBlocksJob
 from ethereumetl.jobs.exporters.blocks_and_transactions_item_exporter import blocks_and_transactions_item_exporter
-from ethereumetl.thread_local_proxy import ThreadLocalProxy
-from tests.ethereumetl.job.mock_batch_web3_provider import MockBatchWeb3Provider
-from tests.helpers import compare_lines_ignore_order, read_file
+from tests.ethereumetl.job.helpers import get_web3_provider
+from tests.helpers import compare_lines_ignore_order, read_file, skip_if_slow_tests_disabled
 
 RESOURCE_GROUP = 'test_export_blocks_job'
 
@@ -37,19 +35,23 @@ def read_resource(resource_group, file_name):
     return tests.resources.read_resource([RESOURCE_GROUP, resource_group], file_name)
 
 
-@pytest.mark.parametrize("start_block,end_block,batch_size,resource_group", [
-    (0, 0, 1, 'block_without_transactions'),
-    (483920, 483920, 1, 'block_with_logs'),
-    (47218, 47219, 1, 'blocks_with_transactions'),
-    (47218, 47219, 2, 'blocks_with_transactions')
+@pytest.mark.parametrize("start_block,end_block,batch_size,resource_group,web3_provider_type", [
+    (0, 0, 1, 'block_without_transactions', 'mock'),
+    (483920, 483920, 1, 'block_with_logs', 'mock'),
+    (47218, 47219, 1, 'blocks_with_transactions', 'mock'),
+    (47218, 47219, 2, 'blocks_with_transactions', 'mock'),
+    skip_if_slow_tests_disabled((0, 0, 1, 'block_without_transactions', 'infura')),
+    skip_if_slow_tests_disabled((483920, 483920, 1, 'block_with_logs', 'infura')),
+    skip_if_slow_tests_disabled((47218, 47219, 2, 'blocks_with_transactions', 'infura')),
 ])
-def test_export_blocks_job(tmpdir, start_block, end_block, batch_size, resource_group):
+def test_export_blocks_job(tmpdir, start_block, end_block, batch_size, resource_group, web3_provider_type):
     blocks_output_file = tmpdir.join('actual_blocks.csv')
     transactions_output_file = tmpdir.join('actual_transactions.csv')
 
+    web3_provider = get_web3_provider(web3_provider_type, lambda file: read_resource(resource_group, file))
     job = ExportBlocksJob(
         start_block=start_block, end_block=end_block, batch_size=batch_size,
-        batch_web3_provider=ThreadLocalProxy(lambda: MockBatchWeb3Provider(lambda file: read_resource(resource_group, file))),
+        batch_web3_provider=web3_provider,
         max_workers=5,
         item_exporter=blocks_and_transactions_item_exporter(blocks_output_file, transactions_output_file),
         export_blocks=blocks_output_file is not None,
