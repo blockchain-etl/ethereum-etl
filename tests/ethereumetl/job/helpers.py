4c2f918864ae4b62b8c3e75e2903c0bfdec965e7
Evgeny Medvedev <medvedev1088@gmail.com>
2018-08-04 00:10:02 +0700

    Add Infura tests

--- tests/ethereumetl/job/helpers.py
+++ tests/ethereumetl/job/helpers.py
@@ -7,11 +7,9 @@ def get_web3_provider(provider_type, read_resource_lambda=None):
     if provider_type == 'mock':
         if read_resource_lambda is None:
             raise ValueError('read_resource_lambda must not be None for provider type mock'.format(provider_type))
-        provider = ThreadLocalProxy(
-            lambda: MockBatchWeb3Provider(read_resource_lambda))
+        provider = ThreadLocalProxy(lambda: MockBatchWeb3Provider(read_resource_lambda))
     elif provider_type == 'infura':
-        provider = ThreadLocalProxy(
-            lambda: BatchHTTPProvider('https://mainnet.infura.io/'))
+        provider = ThreadLocalProxy(lambda: BatchHTTPProvider('https://mainnet.infura.io/'))
     else:
         raise ValueError('Provider type {} is unexpected'.format(provider_type))
     return provider
