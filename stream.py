d677d442bdac45ceaa0213625d1a114e810452c1
Evgeny Medvedev <medvedev1088@gmail.com>
2018-09-27 23:49:36 +0700

    Add enrichment to streaming.py

--- stream.py
+++ stream.py
@@ -22,8 +22,10 @@
 
 
 import argparse
+import itertools
 import logging
 import time
+from collections import defaultdict
 
 from google.api_core.exceptions import GoogleAPIError
 from web3 import Web3
@@ -61,6 +63,120 @@ def read_last_synced_block(file):
 def write_last_synced_block(file, last_synced_block):
     with smart_open(file, 'w') as last_synced_block_file:
         return last_synced_block_file.write(str(last_synced_block) + '\n')
+
+
+def join(left, right, join_fields, left_fields, right_fields):
+    left_join_field, right_join_field = join_fields
+
+    def field_list_to_dict(field_list):
+        result_dict = {}
+        for field in field_list:
+            if isinstance(field, tuple):
+                result_dict[field[0]] = field[1]
+            else:
+                result_dict[field] = field
+        return result_dict
+
+    left_fields_as_dict = field_list_to_dict(left_fields)
+    right_fields_as_dict = field_list_to_dict(right_fields)
+
+    left_map = defaultdict(list)
+    for item in left: left_map[item[left_join_field]].append(item)
+
+    right_map = defaultdict(list)
+    for item in right: right_map[item[right_join_field]].append(item)
+
+    for key in left_map.keys():
+        for left_item, right_item in itertools.product(left_map[key], right_map[key]):
+            result_item = {}
+            for src_field, dst_field in left_fields_as_dict.items():
+                result_item[dst_field] = left_item.get(src_field)
+            for src_field, dst_field in right_fields_as_dict.items():
+                result_item[dst_field] = right_item.get(src_field)
+
+            yield result_item
+
+
+def enrich_transactions(blocks, transactions, receipts):
+    transactions_and_receipts = join(
+        transactions, receipts, ('hash', 'transaction_hash'),
+        left_fields=[
+            'hash',
+            'nonce',
+            'transaction_index',
+            'from_address',
+            'to_address',
+            'value',
+            'gas',
+            'gas_price',
+            'input',
+            'block_number'
+        ],
+        right_fields=[
+            ('cumulative_gas_used', 'receipt_cumulative_gas_used'),
+            ('gas_used', 'receipt_gas_used'),
+            ('contract_address', 'receipt_contract_address'),
+            ('root', 'receipt_root'),
+            ('status', 'receipt_status')
+        ])
+
+    result = join(
+        transactions_and_receipts, blocks, ('block_number', 'number'),
+        [
+            'hash',
+            'nonce',
+            'transaction_index',
+            'from_address',
+            'to_address',
+            'value',
+            'gas',
+            'gas_price',
+            'input',
+            'block_number'
+        ],
+        [
+            ('timestamp', 'block_timestamp'),
+            ('hash', 'block_hash'),
+        ])
+    return list(result)
+
+
+def enrich_logs(blocks, logs):
+    result = join(
+        logs, blocks, ('block_number', 'number'),
+        [
+            'log_index',
+            'transaction_hash',
+            'transaction_index',
+            'address',
+            'data',
+            'topics',
+            'block_number'
+        ],
+        [
+            ('timestamp', 'block_timestamp'),
+            ('hash', 'block_hash'),
+        ])
+    return list(result)
+
+
+def enrich_token_transfers(blocks, token_transfers):
+    result = join(
+        token_transfers, blocks, ('block_number', 'number'),
+        [
+            'token_address',
+            'from_address',
+            'to_address',
+            'value',
+            'transaction_hash',
+            'log_index',
+            'block_number'
+        ],
+        [
+            ('timestamp', 'block_timestamp'),
+            ('hash', 'block_hash'),
+        ])
+    return list(result)
 
 
 max_batch_size = 10
@@ -130,8 +246,18 @@ while True:
         token_transfers_job.run()
         token_transfers = token_transfers_item_exporter.get_items('token_transfer')
 
+        enriched_transactions = enrich_transactions(blocks, transactions, receipts)
+        if len(enriched_transactions) == len(transactions):
+            raise ValueError('The number of transactions is wrong ' + str(enriched_transactions))
+        enriched_logs = enrich_logs(blocks, logs)
+        if len(enriched_logs) != len(logs):
+            raise ValueError('The number of logs is wrong ' + str(enriched_logs))
+        enriched_token_transfers = enrich_token_transfers(blocks, token_transfers)
+        if len(enriched_token_transfers) != len(token_transfers):
+            raise ValueError('The number of token transfers is wrong ' + str(enriched_token_transfers))
+
         logging.info('Publishing to PubSub')
-        pubsub_item_exporter.export_items(blocks + transactions + receipts + logs + token_transfers)
+        pubsub_item_exporter.export_items(blocks + enriched_transactions + enriched_logs + enriched_token_transfers)
 
         logging.info('Writing last synced block {}'.format(target_block))
         write_last_synced_block(args.last_synced_block_file, target_block)
