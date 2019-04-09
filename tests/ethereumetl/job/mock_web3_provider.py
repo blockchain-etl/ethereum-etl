4ed17d49809343b5d8f6fd3bddaba41aa661b07c
Evgeny Medvedev <medvedev1088@gmail.com>
2019-04-09 22:25:22 +0700

    Refactor mock file naming

--- tests/ethereumetl/job/mock_web3_provider.py
+++ tests/ethereumetl/job/mock_web3_provider.py
@@ -31,15 +31,19 @@ class MockWeb3Provider(IPCProvider):
         self.read_resource = read_resource
 
     def make_request(self, method, params):
-        if method == 'eth_call':
-            to = params[0]['to'].lower()
-            data = params[0]['data']
-            file_name = '{}_{}_{}.json'.format(method, to, data)
-        # TODO: Remove this when this issue is fixed
-        # https://github.com/paritytech/parity-ethereum/issues/9822
-        elif method == 'trace_block':
-            file_name = 'trace_filter.json'
-        else:
-            file_name = method + '.json'
+        file_name = build_file_name(method, params)
         file_content = self.read_resource(file_name)
         return json.loads(file_content)
+
+
+def build_file_name(method, params):
+    return 'web3_response.' + method + '_' + '_'.join([param_to_str(param) for param in params]) + '.json'
+
+
+def param_to_str(param):
+    if isinstance(param, dict):
+        return '_'.join([str(key) + '_' + param_to_str(param[key]) for key in sorted(param)])
+    elif isinstance(param, list):
+        return '_'.join([param_to_str(param_item) for param_item in param])
+    else:
+        return str(param).lower()
