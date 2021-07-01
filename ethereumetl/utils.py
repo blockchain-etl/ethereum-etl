24a4716b7cb07a819730b4766b7b760f042905fa
psych0xpomp <76981884+psych0xpomp@users.noreply.github.com>
2021-07-01 12:03:06 +1000

    Update utils.py to fix hex_to_dec
    hex_string is an int, can only perform int conversion with specified base on strings as per the following error:
    ```TypeError: int() can't convert non-string with explicit base```
    Solution: convert hex_string to a string for conversion.

--- ethereumetl/utils.py
+++ ethereumetl/utils.py
@@ -31,7 +31,7 @@ def hex_to_dec(hex_string):
     if hex_string is None:
         return None
     try:
-        return int(hex_string, 16)
+        return int(str(hex_string), 16)
     except ValueError:
         print("Not a hex string %s" % hex_string)
         return hex_string
