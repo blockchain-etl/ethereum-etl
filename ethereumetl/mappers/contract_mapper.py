c208e4ea29db263c2750a4093c8934d5f032ff7c
Phillip LeBlanc <phillip@leblanc.tech>
2022-01-21 12:47:25 +0900

    Remove the large fields

--- ethereumetl/mappers/contract_mapper.py
+++ ethereumetl/mappers/contract_mapper.py
@@ -29,7 +29,6 @@ class EthContractMapper(object):
     def rpc_result_to_contract(self, contract_address, rpc_result):
         contract = EthContract()
         contract.address = contract_address
-        contract.bytecode = rpc_result
 
         return contract
 
@@ -37,7 +36,6 @@ class EthContractMapper(object):
         return {
             'type': 'contract',
             'address': contract.address,
-            'bytecode': contract.bytecode,
             'function_sighashes': contract.function_sighashes,
             'is_erc20': contract.is_erc20,
             'is_erc721': contract.is_erc721,
--- ethereumetl/mappers/transaction_mapper.py
+++ ethereumetl/mappers/transaction_mapper.py
@@ -39,7 +39,6 @@ class EthTransactionMapper(object):
         transaction.value = hex_to_dec(json_dict.get('value'))
         transaction.gas = hex_to_dec(json_dict.get('gas'))
         transaction.gas_price = hex_to_dec(json_dict.get('gasPrice'))
-        transaction.input = json_dict.get('input')
         transaction.max_fee_per_gas = hex_to_dec(json_dict.get('maxFeePerGas'))
         transaction.max_priority_fee_per_gas = hex_to_dec(json_dict.get('maxPriorityFeePerGas'))
         transaction.transaction_type = hex_to_dec(json_dict.get('type'))
@@ -59,7 +58,6 @@ class EthTransactionMapper(object):
             'value': transaction.value,
             'gas': transaction.gas,
             'gas_price': transaction.gas_price,
-            'input': transaction.input,
             'max_fee_per_gas': transaction.max_fee_per_gas,
             'max_priority_fee_per_gas': transaction.max_priority_fee_per_gas,
             'transaction_type': transaction.transaction_type
