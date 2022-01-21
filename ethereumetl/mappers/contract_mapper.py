cd95d19b828eec57e75e203709364ad46576a233
Phillip LeBlanc <phillip@leblanc.tech>
2022-01-21 13:40:36 +0900

    add back mapper

--- ethereumetl/mappers/contract_mapper.py
+++ ethereumetl/mappers/contract_mapper.py
@@ -29,6 +29,7 @@ class EthContractMapper(object):
     def rpc_result_to_contract(self, contract_address, rpc_result):
         contract = EthContract()
         contract.address = contract_address
+        contract.bytecode = rpc_result
 
         return contract
 
@@ -36,6 +37,7 @@ class EthContractMapper(object):
         return {
             'type': 'contract',
             'address': contract.address,
+            'bytecode': contract.bytecode,
             'function_sighashes': contract.function_sighashes,
             'is_erc20': contract.is_erc20,
             'is_erc721': contract.is_erc721,
