7dd4c83f32c95e63727d0c9dd560d138040d8ac5
Maxim Razhev <cvlt.mao@gmail.com>
2022-03-11 13:17:16 +0500

    Fix import in eth service test

--- tests/ethereumetl/service/test_eth_service.py
+++ tests/ethereumetl/service/test_eth_service.py
@@ -27,6 +27,7 @@ from web3 import HTTPProvider, Web3
 
 from ethereumetl.service.eth_service import EthService
 from ethereumetl.service.graph_operations import OutOfBoundsError
+from ethereumetl.web3_utils import build_web3
 from tests.helpers import skip_if_slow_tests_disabled
 
 
