c45f1956d9cef08599fcb9e9577b45289afa3ab0
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2022-04-12 19:20:09 -0700

    bump pulsar version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
             'timeout-decorator==0.4.1',
             'google-cloud-pubsub==2.1.0',
             'google-cloud-storage==1.33.0',
-            'pulsar==2.7.0',
+            'pulsar-client==2.10.0',
             'kafka-python==2.0.2',
             'sqlalchemy==1.4',
             'pg8000==1.13.2',
