d1c91a85fbd723d18a75089dac0796d05a47d49e
Phillip LeBlanc <phillip@leblanc.tech>
2022-05-03 12:01:48 +0900

    v1.0.18

--- ethereumetl/cli/__init__.py
+++ ethereumetl/cli/__init__.py
@@ -46,7 +46,7 @@ logging_basic_config()
 
 
 @click.group()
-@click.version_option(version='1.11.0-spicehq/release/v1.0.17')
+@click.version_option(version='1.11.0-spicehq/release/v1.0.18')
 @click.pass_context
 def cli(ctx):
     pass
