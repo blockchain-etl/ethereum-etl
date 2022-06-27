197bc801261ef83943703765b037129200d77749
saurabhdaga-merkle <saurabh@merklescience.com>
2022-06-28 00:38:16 +0530

    add token_address key

--- start-hot.sh
+++ start-hot.sh
@@ -2,4 +2,4 @@
 
 rm last_synced_block_hot.txt
 
-python ethereumetl.py stream --provider-uri http://eth-fast.palantree.com:8545/ -w 15 -B 20 -b 5 --period-seconds 2 -l last_synced_block_hot.txt -s $1 -e transaction,token_transfer,trace -o kafka -t eth.hot
+python ethereumetl.py stream --provider-uri http://eth-fast.palantree.com:8545/ -w 35 -B 20 -b 5 --period-seconds 1 -l last_synced_block_hot.txt -s $1 -e transaction,token_transfer,trace -o kafka -t eth.hot
--- start-warm.sh
+++ start-warm.sh
@@ -2,4 +2,4 @@
 
 rm last_synced_block_warm.txt
 
-python ethereumetl.py stream --provider-uri http://eth-fast.palantree.com:8545/ -w 15 -b 10 -B 10 --period-seconds 5  -l last_synced_block_warm.txt -s $1 --lag 18 -e transaction,token_transfer -o kafka  -t eth.hot
+python ethereumetl.py stream --provider-uri http://eth-fast.palantree.com:8545/ -w 35 -b 10 -B 10 --period-seconds 5  -l last_synced_block_warm.txt -s $1 --lag 18 -e transaction,token_transfer -o kafka  -t eth.hot
