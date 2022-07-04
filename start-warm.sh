7d5427b593109e3016b5dd176600aeccf5630049
saurabhdaga-merkle <saurabh@merklescience.com>
2022-07-04 11:12:46 +0530

    warm

--- start-warm.sh
+++ start-warm.sh
@@ -2,4 +2,4 @@
 
 rm last_synced_block_warm.txt
 
-python ethereumetl.py stream --provider-uri http://eth-fast.palantree.com:8545/ -w 35 -b 10 -B 10 --period-seconds 5  -l last_synced_block_warm.txt -s $1 --lag 18 -e transaction,token_transfer -o kafka  -t eth.hot
+python ethereumetl.py stream --provider-uri https://patient-quiet-water.quiknode.pro/cbf46386e585531f903652184d07ca49e58cddaa -w 5 -B 20 -b 5 --period-seconds 5 -l last_synced_block_warm.txt -s $1 --lag 18 -e transaction,token_transfer,trace -o kafka -t eth.warm
