ac871685576db3623ea38b1003c262a3e3c12053
sleepy-tiger <yanyu@smartcontract.com>
2022-12-05 14:32:04 +1300

    Upgrade eth-abi lower bound to >=2.2.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         'web3>=5.29,<6',
         'eth-utils==1.10',
-        'eth-abi==2.1.1',
+        'eth-abi>=2.2.0,<3.0.0',
         # TODO: This has to be removed when "ModuleNotFoundError: No module named 'eth_utils.toolz'" is fixed at eth-abi
         'python-dateutil>=2.8.0,<3',
         'click==8.0.4',
