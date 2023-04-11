7a729b73206e7c6c364deddd4a2a362efca394db
heeth-arda <heeth@arda.finance>
2023-04-11 17:41:39 +0530

    feat: add extra columns for token_transfer

--- ethereumetl/jobs/extract_token_transfers_job.py
+++ ethereumetl/jobs/extract_token_transfers_job.py
@@ -20,12 +20,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import time
 from ethereumetl.executors.batch_work_executor import BatchWorkExecutor
 from blockchainetl.jobs.base_job import BaseJob
 from ethereumetl.mappers.token_transfer_mapper import EthTokenTransferMapper
 from ethereumetl.mappers.receipt_log_mapper import EthReceiptLogMapper
 from ethereumetl.mappers.transaction_mapper import EthTransactionMapper
 from ethereumetl.service.token_transfer_extractor import EthTokenTransferExtractor
+from multicall import Call, Multicall
+import requests
 
 
 class ExtractTokenTransfersJob(BaseJob):
@@ -79,3 +82,117 @@ class ExtractTokenTransfersJob(BaseJob):
     def _end(self):
         self.batch_work_executor.shutdown()
         self.item_exporter.close()
+
+    def _call_back(success, value):
+        """
+        Callback to process results from multicall for a function.
+        If call fails returns False (if changed to string throws error)
+        """
+        try:
+            if success is True and isinstance(value, bytes):
+                return value.decode("utf-8")
+            elif success is True:
+                return value
+            else:
+                return None
+        except Exception as e:
+            return None
+
+
+    def add_extra_columns_in_token_transfers(self, token_transfers, web3):
+
+        updated_token_transfers = []
+        grouped_token_transfers = {}
+
+        for item in token_transfers:
+            block_number = item['block_number']
+            if block_number in grouped_token_transfers:
+                grouped_token_transfers[block_number].append(item)
+            else:
+                grouped_token_transfers[block_number] = [item]
+
+        for block_number, transfers in grouped_token_transfers.items():
+            
+            token_addresses = {}
+            multicall_balance_of_calls = []
+            for transfer in transfers:
+                token_addresses[transfer['token_address']] = ''
+                multicall_balance_of_calls.append(
+                    Call(
+                        transfer['token_address'], 
+                        ['balanceOf(address)(uint256)', transfer['from_address']], 
+                        [(f"{transfer['from_address']}.balanceOf", self._call_back)]
+                    )
+                )
+
+            multicall_token_details_calls = []
+            unique_token_addresses = list(token_addresses)
+            for token_address in unique_token_addresses:
+                multicall_token_details_calls.extend(
+                    [
+                        Call(
+                            token_address, 
+                            ['totalSupply()(uint256)'],
+                            [(f"{token_address}.totalSupply", self._call_back)]
+                        ),
+                        Call(
+                            token_address, 
+                            ['decimals()(uint8)'],
+                            [(f"{token_address}.decimals", self._call_back)]
+                        ),
+                        Call(
+                            token_address, 
+                            ['symbol()(string)'],
+                            [(f"{token_address}.symbol", self._call_back)]
+                        )
+                    ]
+                )
+
+
+
+            token_address_groups = [unique_token_addresses[i:i+5] for i in range(0, len(unique_token_addresses), 5)]
+
+            block_timestamp = transfers[0].block_timestamp
+            if time.time() - block_timestamp > 3600 * 6:
+                defillama_url = "https://coins.llama.fi/prices/current/"
+            else:
+                defillama_url = f"https://coins.llama.fi/prices/historical/{block_timestamp}/"
+            
+            token_price_dict = {}
+
+            for group in token_address_groups:
+                request_url = defillama_url + ','.join([f"ethereum:{address}" for address in group])
+                response = requests.get(request_url)
+
+                if response.status_code == 200:
+                    coins = response.json()['coins']
+                    for key, value in coins.items():
+                        address = key[len("ethereum:"):].lower()
+                        token_price_dict[address] = value['price']
+
+            previous_block = block_number - 1 if block_number != 0 else 0
+
+            balance_of_multi_call = Multicall(multicall_balance_of_calls, _w3=web3, require_success=False, block_id=previous_block)
+            balance_of_multicall_result = balance_of_multi_call()
+
+            token_details_multi_call = Multicall(multicall_token_details_calls, _w3=web3, require_success=False, block_id=previous_block)
+            token_details_multicall_result = token_details_multi_call()
+
+            for transfer in transfers:
+                
+                previous_block_balance_of = balance_of_multicall_result[f"{transfer['from_address']}.balanceOf"]
+                previous_block_total_supply = token_details_multicall_result[f"{transfer['token_address']}.totalSupply"]
+                token_symbol = token_details_multicall_result[f"{transfer['token_address']}.symbol"]
+                token_decimals = token_details_multicall_result[f"{transfer['token_address']}.decimals"]
+                token_price = token_address_groups[transfer['token_address']]
+
+                updated_token_transfers.append({
+                    **transfer,
+                    'previous_block_token_balance_of': previous_block_balance_of,
+                    'previous_block_token_total_supply': previous_block_total_supply,
+                    'symbol': token_symbol,
+                    'decimals': token_decimals,
+                    'price': token_price
+                })
+        
+        return updated_token_transfers
