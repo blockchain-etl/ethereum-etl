952a49ba4b53afc9b6e2c73cd42616ac1870d9c6
medvedev1088 <medvedev1088@gmail.com>
2023-08-25 17:14:23 +0700

    Bump version

--- ethereumetl/cli/__init__.py
+++ ethereumetl/cli/__init__.py
@@ -48,7 +48,7 @@ from ethereumetl.cli.stream import stream
 
 
 @click.group()
-@click.version_option(version='2.3.0')
+@click.version_option(version='2.3.1')
 @click.pass_context
 def cli(ctx):
     pass
